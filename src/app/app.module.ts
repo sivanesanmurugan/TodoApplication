0a2d506b45f2d977cc5b47780b6d0587a96bcd3a
sivanesanmurugan <sivanesan.murugan@kumaran.com>
2023-10-18 18:06:02 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+}) 
+export class TodoComponent {
+  emailRef: string = '';
+  nameRef: string = '';
+  numberRef: string = '';
+  todos: Todo[] = [];
+  Add: string = 'Add';
+  editid: number = 0;
+  add() {
+    if (this.editid === 0) {
+      let todo = {
+        id: this.todos.length + 1,
+        name: this.nameRef,
+        email: this.emailRef,
+        phonenumber: this.numberRef,
+      };
+      this.todos.push(todo);
+    } else {
+      let todonew = this.todos.map((todo) => {
+        if (todo.id === this.editid) {
+          return {
+            ...todo,
+            id: this.editid,
+            name: this.nameRef,
+            email: this.emailRef,
+            phonenumber: this.numberRef,
+          };
+        } else {
+          return todo;
+        }
+      });
+      this.todos = todonew;
+      this.Add = 'Add';
+      this.editid = 0;
+    }
+    this.emailRef = '';
+    this.nameRef = '';
+    this.numberRef = '';
+  }
+  edit(id: number) {
+    this.editid = id;
+    this.Add = 'Edit';
+    let Newtodo = this.todos.find((todo) => todo.id == id);
+    if (Newtodo) {
+      this.nameRef = Newtodo.name;
+      this.emailRef = Newtodo.email;
+      this.numberRef = Newtodo.phonenumber;
+    }
+  }
+  delete(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+}
